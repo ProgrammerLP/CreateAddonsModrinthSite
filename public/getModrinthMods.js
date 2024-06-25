18163b78936ac44ac4c0b7bf3f5ec301df53db9d
ProgrammerLP <101939228+ProgrammerLP@users.noreply.github.com>
2024-06-25 05:55:20 +0000

    api url

--- public/getModrinthMods.js
+++ public/getModrinthMods.js
@@ -1,6 +1,6 @@
 export async function getModrinthMods() {
     try {
-      const response = await fetch("https://www.adeptstack.net/News.json");
+      const response = await fetch("https://api.modrinth.com/v2/search?limit=100&facets=[[%22project_type:mod%22]]&offset=100&query=create");
       const newsData = await response.json();
       return newsData;
     } catch (error) {
